4e94fb1bb59d10899c875e6c4e9161697cbe542d
Ivan <Fuck3327@mail.ru>
2025-03-06 12:16:21 +0300

    add delete get methods and array

--- index.js
+++ index.js
@@ -6,19 +6,46 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-app.post("/data", (req, res) => {
-  let { id, state } = req.body;
-  let currentDate = new Date().toLocaleString("ru-RU", {
-    timeZone: "Europe/Moscow",
-  });
-  let formingDataObj = {
-    id,
-    state,
-    currentDate,
-  };
-  console.log("Полученные данные:", formingDataObj);
+let motionDataArr = [];
 
-  return res.json({ message: "Данные получены", currentDate });
+app.delete("/data", (req, res) => {
+  try {
+    let { objId } = req.body;
+    let resultSearch = motionDataArr.filter((item) => item.objId !== objId);
+    motionDataArr = resultSearch;
+    return res.json(motionDataArr);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/data", (req, res) => {
+  try {
+    res.json(motionDataArr);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/data", (req, res) => {
+  try {
+    let { deviceId, state } = req.body;
+    let currentDate = new Date().toLocaleString("ru-RU", {
+      timeZone: "Europe/Moscow",
+    });
+    let formingDataObj = {
+      deviceId,
+      objId: Math.random().toString().substring(2, 8),
+      state,
+      currentDate,
+    };
+    motionDataArr.push(formingDataObj);
+    console.log("Полученные данные:", formingDataObj);
+
+    return res.json({ message: "Данные получены", currentDate });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // app.get("/data", (req, res) => {
